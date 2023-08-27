abed3ac8b3185a6006f0c31fbf97aadb5df5449d
EDWIN GEORGE LAZER <edwin04@gmail.com>
2023-08-27 06:01:25 +0100

    Build Search Query

--- src/App.js
+++ src/App.js
@@ -6,25 +6,30 @@ import RepoInfo from './RepoInfo.js';
 function App() {
   const [username, setUsername] = useState('');
   const [repoList, setRepoList] = useState(null);
+  const [pageCount, setPageCount] = useState(10);
+  const [queryString, setQueryString] = useState('slides');
+  const [totalCount, setTotalCount] = useState(null);
 
   const effectRan = useRef(false);
 
   const fetchData = useCallback(() => {
+    const queryText = JSON.stringify(gitHubQuery(pageCount, queryString));
     fetch(gitHub.baseURL, {
       method: 'POST',
       headers: gitHub.headers,
-      body: JSON.stringify(gitHubQuery),
+      body: queryText,
     })
       .then((response) => response.json())
       .then((data) => {
         const viewer = data.data.viewer;
         const repos = data.data.search.nodes;
+        const total = data.data.search.repositoryCount;
         setUsername(viewer.name);
-        // setRepoList(viewer.repositories.nodes);
         setRepoList(repos);
+        setTotalCount(total);
       })
       .catch((err) => console.log(err));
-  }, []);
+  }, [pageCount, queryString]);
 
   useEffect(() => {
     if (effectRan.current === false) {
@@ -41,6 +46,12 @@ function App() {
         <i className='bi bi-diagram-2-fill'></i>Repos
       </h1>
       <p>Hey there {username}</p>
+      <p>
+        <b>Search for: </b>
+        {queryString} | <b>Items per page: </b>
+        {pageCount} | <b>Total results: </b>
+        {totalCount}
+      </p>
       {repoList && (
         <ul className='list-group list-group-flush'>
           {repoList.map((repo) => (
--- src/Query.js
+++ src/Query.js
@@ -1,29 +1,32 @@
-const gitHubQuery = {
-  query: `
-  {
-    viewer {
-      name
-    }
-    search(
-      query: "tutorial user:planetoftheweb sort:updated-desc"
-      type: REPOSITORY
-      first: 100
-    ) {
-      nodes {
-        ... on Repository {
-          name
-          description
-          id
-          url
-          licenseInfo {
-            spdxId
+const gitHubQuery = (pageCount, queryString) => {
+  return {
+    query: `
+    {
+      viewer {
+        name
+      }
+      search(
+        query: "${queryString} user:planetoftheweb sort:updated-desc"
+        type: REPOSITORY
+        first: ${pageCount}
+      ) {
+        repositoryCount
+        nodes {
+          ... on Repository {
+            name
+            description
+            id
+            url
+            licenseInfo {
+              spdxId
+            }
+            viewerSubscription
           }
-          viewerSubscription
         }
       }
     }
-  }
-    `,
+      `,
+  };
 };
 
 export default gitHubQuery;
